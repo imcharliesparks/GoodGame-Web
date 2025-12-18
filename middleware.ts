7337691a4c3cc42cc560e79068eb734bf7036da7
imcharliesparks <sparksfullstack@gmail.com>
2025-12-18 17:54:19 -0500

    removed old middleware file

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// Use default Clerk middleware to hydrate auth() for route handlers.
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/api/(.*)"],
-};
